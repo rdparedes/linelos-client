78cd730d55bee8c11327675a63a5d55ec11c93e0
Roberto Paredes <rparedes@thoughtworks.com>
2018-02-27 00:41:35 -0300

    changes prod api url

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const env = process.env;
 let linelosApiUrl = "http://localhost:3478";
 
 if (env.PRODUCTION) {
-  linelosApiUrl = "http://ec2-18-221-97-203.us-east-2.compute.amazonaws.com";
+  linelosApiUrl = "https://linelos.herokuapp.com";
 }
 
 export { linelosApiUrl };
