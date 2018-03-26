72ef0f90480e33368f5f7eaffd7abd2280d16513
Roberto Paredes <rparedes@thoughtworks.com>
2018-03-25 23:40:38 -0500

    adds formatting to transaction output

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+const formatAsMoney = monto => `\$${monto.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,")}`;
+const formatAsDate = fecha => new Date(fecha).toLocaleDateString("es-EC");
+
 const Transaction = ({ tienda, monto, fecha }) => (
   <tr>
     <td>{tienda}</td>
-    <td>{monto}</td>
-    <td>{fecha}</td>
+    <td>{formatAsMoney(monto)}</td>
+    <td>{formatAsDate(fecha)}</td>
   </tr>
 );
 
